26ec6d2344932e590510a57e26c900a9df8ec07d
jagadeesh-chinta <chintuchinta25@gmail.com>
2026-09-12 13:36:49 +0530

    Bind Express server host to 0.0.0.0

--- server/server.js
+++ server/server.js
@@ -65,7 +65,7 @@ app.use((err, req, res, next) => {
   });
 });
 
-const server = app.listen(PORT, () => {
+const server = app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
 
